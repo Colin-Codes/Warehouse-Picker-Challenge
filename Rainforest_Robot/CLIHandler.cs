f4d9d77ed92881807a8d86169e2d110216422193
Colin <cy16aan@herts.ac.uk>
2020-11-28 09:42:37 +0000

    Hellow World

---
+++ Rainforest_Robot/CLIHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Rainforest_Robot
+{
+    class CLIHandler
+    {
+        static void Main(string[] args)
+        {
+            bool testMode = false;
+            bool humanMode = false;
+            foreach (string arg in args) {
+                if (arg == "--TEST") {
+                    testMode = true;
+                }
+                else if (arg == "--HUMAN") {
+                    humanMode = true;
+                }
+            }
+            // Interpret command line arguments
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+// Robot picks up one at a time from crate
+// Drops all at once into feeder
+
+// Instructions one at a time
+// Robot moves NSEW one at a time
+// Pick up - P
+//   If not at a crate, it falls over
+// Drop off - D
+//  If not at a feeder it falls over
